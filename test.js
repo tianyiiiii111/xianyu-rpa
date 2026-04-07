6c4d43e421ebf6945b56a917b276562eac142a45
tianyiiiii111 <yit47719@gmail.com>
2026-04-07 18:12:11 +0800

    Change test.js

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ import { checkForbiddenWords } from './src/services/AiServes.js';
 import { StorageService } from './src/utils/storage.js';
 import { loginAndSaveCookie } from './src/services/LoginServies.js';  
 
-// const cookieName = await loginAndSaveCookie();
-// console.log(cookieName);
+const cookieName = await loginAndSaveCookie();
+console.log(cookieName);
 
 // 测试搜索商品
 // searchProducts('Ai接口','huazhongkandianyingdehaitunsha').then(results => {
@@ -14,9 +14,9 @@ import { loginAndSaveCookie } from './src/services/LoginServies.js';
 // });
 
 // 测试批量发布商品
-batchPublish('search_1775453373756_Ai接口', 'dadishangdeyipianyezi').then(results => {
-  console.log(results);
-});
+// batchPublish('search_1775453373756_Ai接口', 'dadishangdeyipianyezi').then(results => {
+//   console.log(results);
+// });
 
 // AI 审核商品描述
 // const searchResults = StorageService.getSearchResults('search_1775453373756_Ai接口');
